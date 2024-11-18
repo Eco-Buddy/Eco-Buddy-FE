aa3c774d364008ea1f01fb39c8e5888d02f4e397
son0307 <son03071@naver.com>
2024-11-18 13:43:46 +0900

    Revert "login"
    This reverts commit 92e32daeed0a61b8e6d78a92a0ffbca18ae64876.

--- lib/main.dart
+++ lib/main.dart
@@ -1,262 +1,125 @@
 import 'package:flutter/material.dart';
-import 'package:webview_flutter/webview_flutter.dart';
-import 'dart:convert';
-import 'package:http/http.dart' as http;
 
-void main() => runApp(MyApp());
+void main() {
+  runApp(const MyApp());
+}
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Naver & Kakao Login App',
+      title: 'Flutter Demo',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        // This is the theme of your application.
+        //
+        // TRY THIS: Try running your application with "flutter run". You'll see
+        // the application has a purple toolbar. Then, without quitting the app,
+        // try changing the seedColor in the colorScheme below to Colors.green
+        // and then invoke "hot reload" (save your changes or press the "hot
+        // reload" button in a Flutter-supported IDE, or press "r" if you used
+        // the command line to start the app).
+        //
+        // Notice that the counter didn't reset back to zero; the application
+        // state is not lost during the reload. To reset the state, use hot
+        // restart instead.
+        //
+        // This works for code too, not just values: Most code changes can be
+        // tested with just a hot reload.
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
       ),
-      home: NaverLoginScreen(),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
 
-class NaverLoginScreen extends StatefulWidget {
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
+
   @override
-  _NaverLoginScreenState createState() => _NaverLoginScreenState();
+  State<MyHomePage> createState() => _MyHomePageState();
 }
 
-class _NaverLoginScreenState extends State<NaverLoginScreen> {
-  late WebViewController _controller;
-  bool _isWebViewVisible = false;
-  final String naverServerUrl = 'http://223.130.162.100:4525/login/request/naver';
-  final String kakaoServerUrl = 'http://223.130.162.100:4525/login/request/kakao';
-  final String naverLogoutServerUrl = 'http://223.130.162.100:4525/logout/naver';
-  final String kakaoLogoutServerUrl = 'http://223.130.162.100:4525/logout/kakao';
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
 
-  String? naverUserId;
-  String? naverAccessToken;
-  String? kakaoUserId;
-  String? kakaoAccessToken;
-
-  @override
-  void initState() {
-    super.initState();
-    _controller = WebViewController()
-      ..setJavaScriptMode(JavaScriptMode.unrestricted)
-      ..setNavigationDelegate(
-        NavigationDelegate(
-          onNavigationRequest: (NavigationRequest request) {
-            if (request.url.startsWith(
-                "http://223.130.162.100:4525/login/oauth2/code/naver")) {
-              _handleRedirectUri(request.url, isNaver: true);
-              return NavigationDecision.prevent;
-            } else if (request.url.startsWith(
-                "http://223.130.162.100:4525/login/oauth2/code/kakao")) {
-              _handleRedirectUri(request.url, isNaver: false);
-              return NavigationDecision.prevent;
-            }
-            return NavigationDecision.navigate;
-          },
-        ),
-      );
-  }
-
-  // 네이버 로그인 페이지 로드
-  void _loadNaverLoginPage() async {
+  void _incrementCounter() {
     setState(() {
-      _isWebViewVisible = true;
+      // This call to setState tells the Flutter framework that something has
+      // changed in this State, which causes it to rerun the build method below
+      // so that the display can reflect the updated values. If we changed
+      // _counter without calling setState(), then the build method would not be
+      // called again, and so nothing would appear to happen.
+      _counter++;
     });
-
-    final response = await http.get(Uri.parse(naverServerUrl));
-    if (response.statusCode == 200) {
-      String naverLoginUrl = response.body;
-      _controller.loadRequest(Uri.parse(naverLoginUrl));
-    } else {
-      print('네이버 로그인 URL 로딩 실패');
-    }
-  }
-
-  // 카카오 로그인 페이지 로드
-  void _loadKakaoLoginPage() async {
-    setState(() {
-      _isWebViewVisible = true;
-    });
-
-    final response = await http.get(Uri.parse(kakaoServerUrl));
-    if (response.statusCode == 200) {
-      String kakaoLoginUrl = response.body;
-      _controller.loadRequest(Uri.parse(kakaoLoginUrl));
-    } else {
-      print('카카오 로그인 URL 로딩 실패');
-    }
-  }
-
-  // 네이버 로그아웃
-  void _naverLogout() async {
-    if (naverUserId != null && naverAccessToken != null) {
-      await _clearCookiesAndCache(); // 웹뷰의 쿠키와 캐시를 삭제
-      final response = await http.post(
-        Uri.parse(naverLogoutServerUrl),
-        headers: {"Content-Type": "application/json"},
-        body: jsonEncode({
-          'id': naverUserId,
-          'access_token': naverAccessToken,
-        }),
-      );
-      if (response.statusCode == 200) {
-        setState(() {
-          naverUserId = null;
-          naverAccessToken = null;
-        });
-        print('네이버 로그아웃 성공');
-      } else {
-        print('네이버 로그아웃 실패');
-      }
-    } else {
-      print('네이버 로그인 정보가 없습니다.');
-    }
-  }
-
-  // 카카오 로그아웃
-  void _kakaoLogout() async {
-    if (kakaoUserId != null && kakaoAccessToken != null) {
-      await _clearCookiesAndCache(); // 웹뷰의 쿠키와 캐시를 삭제
-      final response = await http.post(
-        Uri.parse(kakaoLogoutServerUrl),
-        headers: {"Content-Type": "application/json"},
-        body: jsonEncode({
-          'id': kakaoUserId,
-          'access_token': kakaoAccessToken,
-        }),
-      );
-      if (response.statusCode == 200) {
-        setState(() {
-          kakaoUserId = null;
-          kakaoAccessToken = null;
-        });
-        print('카카오 로그아웃 성공');
-      } else {
-        print('카카오 로그아웃 실패');
-      }
-    } else {
-      print('카카오 로그인 정보가 없습니다.');
-    }
-  }
-
-  // 쿠키와 캐시 삭제 함수
-  Future<void> _clearCookiesAndCache() async {
-    await _controller.clearCache();
-    await WebViewCookieManager().clearCookies();
-    print('쿠키와 캐시 삭제 완료');
   }
 
   @override
   Widget build(BuildContext context) {
+    // This method is rerun every time setState is called, for instance as done
+    // by the _incrementCounter method above.
+    //
+    // The Flutter framework has been optimized to make rerunning build methods
+    // fast, so that you can just rebuild anything that needs updating rather
+    // than having to individually change instances of widgets.
     return Scaffold(
-      body: WillPopScope(
-        onWillPop: () async {
-          if (_isWebViewVisible) {
-            // 웹뷰가 열려 있을 때 뒤로 가기를 누르면 웹뷰를 닫음
-            setState(() {
-              _clearCookiesAndCache();
-              _isWebViewVisible = false;
-            });
-            return false; // 앱을 종료하지 않음
-          }
-          return true; // 앱을 종료함
-        },
-        child: Stack(
-          children: [
-            Center(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  const Text(
-                    'ECO Buddy',
-                    style: TextStyle(fontSize: 32,
-                        fontWeight: FontWeight.bold,
-                        color: Colors.green),
-                  ),
-                  const SizedBox(height: 8),
-                  Text(
-                    '디지털 탄소 발자국을 줄여보세요!',
-                    style: TextStyle(fontSize: 16, color: Colors.grey[600]),
-                  ),
-                  const SizedBox(height: 32),
-                  // Kakao login button
-                  GestureDetector(
-                    onTap: () => _loadKakaoLoginPage(),
-                    child: Image.asset(
-                      'assets/kakao_login_btn.png',
-                      // Path to your Kakao login button image
-                      width: 240,
-                    ),
-                  ),
-                  const SizedBox(height: 16),
-                  // Naver login button
-                  GestureDetector(
-                    onTap: () => _loadNaverLoginPage(),
-                    child: Image.asset(
-                      'assets/naver_login_btn.png',
-                      // Path to your Naver login button image
-                      width: 240,
-                    ),
-                  ),
-                  const SizedBox(height: 16),
-                  // Show user ID and access token
-                  if (naverUserId != null || kakaoUserId != null)
-                    Padding(
-                      padding: const EdgeInsets.symmetric(horizontal: 16.0),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          if (naverUserId != null) ...[
-                            Text('Naver User ID: $naverUserId'),
-                            Text('Naver Access Token: $naverAccessToken'),
-                          ],
-                          if (kakaoUserId != null) ...[
-                            Text('Kakao User ID: $kakaoUserId'),
-                            Text('Kakao Access Token: $kakaoAccessToken'),
-                          ],
-                        ],
-                      ),
-                    ),
-                ],
-              ),
+      appBar: AppBar(
+        // TRY THIS: Try changing the color here to a specific color (to
+        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
+        // change color while the other colors stay the same.
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        // Here we take the value from the MyHomePage object that was created by
+        // the App.build method, and use it to set our appbar title.
+        title: Text(widget.title),
+      ),
+      body: Center(
+        // Center is a layout widget. It takes a single child and positions it
+        // in the middle of the parent.
+        child: Column(
+          // Column is also a layout widget. It takes a list of children and
+          // arranges them vertically. By default, it sizes itself to fit its
+          // children horizontally, and tries to be as tall as its parent.
+          //
+          // Column has various properties to control how it sizes itself and
+          // how it positions its children. Here we use mainAxisAlignment to
+          // center the children vertically; the main axis here is the vertical
+          // axis because Columns are vertical (the cross axis would be
+          // horizontal).
+          //
+          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
+          // action in the IDE, or press "p" in the console), to see the
+          // wireframe for each widget.
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'You have pushed the button this many times:',
             ),
-            // 웹뷰가 필요할 때만 표시
-            _isWebViewVisible
-                ? WebViewWidget(controller: _controller)
-                : Container(),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
           ],
         ),
       ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
-  }
-
-
-  // Redirect URI 핸들링
-  void _handleRedirectUri(String url, {required bool isNaver}) async {
-    setState(() {
-      _isWebViewVisible = false;
-    });
-
-    final response = await http.post(Uri.parse(url));
-    if (response.statusCode == 200) {
-      final data = jsonDecode(response.body);
-
-      setState(() {
-        if (isNaver) {
-          naverUserId = data['id'];
-          naverAccessToken = data['access_token'];
-        } else {
-          kakaoUserId = data['id'];
-          kakaoAccessToken = data['access_token'];
-        }
-      });
-
-      print('User ID: ${isNaver ? naverUserId : kakaoUserId}');
-      print('Access Token: ${isNaver ? naverAccessToken : kakaoAccessToken}');
-    } else {
-      print('로그인 완료 후 토큰 데이터 요청 실패');
-    }
   }
 }
