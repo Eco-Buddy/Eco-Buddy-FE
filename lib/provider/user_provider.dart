b32dda88f696cdf594d70071bcf539ce166faa8f
jsy0605 <jungsy0605@naver.com>
2024-12-01 16:48:21 +0900

    ✨window에서 안되던 문제 해결 야호

--- lib/provider/user_provider.dart
+++ lib/provider/user_provider.dart
@@ -1,13 +1,38 @@
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 
 class UserProvider extends ChangeNotifier {
   Map<String, dynamic>? user;
+  final FlutterSecureStorage _secureStorage = FlutterSecureStorage();
+
+  Future<Map<String, String>> _buildHeaders() async {
+    final authorization = await _secureStorage.read(key: 'accessToken');
+    final deviceId = await _secureStorage.read(key: 'deviceId');
+    final userId = await _secureStorage.read(key: 'userId');
+
+    if (authorization == null || deviceId == null || userId == null) {
+      throw Exception("Missing required headers");
+    }
+
+    return {
+      'authorization': authorization,
+      'deviceId': deviceId,
+      'userId': userId,
+      'Content-Type': 'application/json',
+    };
+  }
 
   Future<void> fetchUserData() async {
     try {
-      final response = await http.get(Uri.parse('http://223.130.162.100:4525/api/user'));
+      final headers = await _buildHeaders();
+
+      final response = await http.get(
+        Uri.parse('http://223.130.162.100:4525/api/user'),
+        headers: headers,
+      );
+
       if (response.statusCode == 200) {
         user = jsonDecode(response.body);
         notifyListeners();
@@ -23,11 +48,14 @@ class UserProvider extends ChangeNotifier {
     if (user == null) return;
 
     try {
+      final headers = await _buildHeaders();
+
       final response = await http.patch(
         Uri.parse('http://223.130.162.100:4525/api/user/points'),
+        headers: headers,
         body: jsonEncode({'points': points}),
-        headers: {'Content-Type': 'application/json'},
       );
+
       if (response.statusCode == 200) {
         user!['points'] = jsonDecode(response.body)['points'];
         notifyListeners();
@@ -43,11 +71,14 @@ class UserProvider extends ChangeNotifier {
     if (user == null) return;
 
     try {
+      final headers = await _buildHeaders();
+
       final response = await http.patch(
         Uri.parse('http://223.130.162.100:4525/api/user/name'),
+        headers: headers,
         body: jsonEncode({'name': newName}),
-        headers: {'Content-Type': 'application/json'},
       );
+
       if (response.statusCode == 200) {
         user!['nickname'] = jsonDecode(response.body)['nickname'];
         notifyListeners();
@@ -56,6 +87,40 @@ class UserProvider extends ChangeNotifier {
       }
     } catch (e) {
       print('Error updating name: $e');
+    }
+  }
+
+  Future<void> loadPetData() async {
+    try {
+      final headers = await _buildHeaders();
+
+      final response = await http.post(
+        Uri.parse('http://223.130.162.100:4525/pet/load'),
+        headers: headers,
+      );
+
+      if (response.statusCode == 200) {
+        final data = jsonDecode(response.body);
+        print("Pet Name: ${data['petName']}");
+        print("New Access Token: ${data['new_accessToken']}");
+
+        // Update secure storage with new access token if provided
+        if (data.containsKey('new_accessToken')) {
+          await _secureStorage.write(key: 'accessToken', value: data['new_accessToken']);
+        }
+
+        notifyListeners();
+      } else if (response.statusCode == 400) {
+        print("Bad Request: Missing userId or token.");
+      } else if (response.statusCode == 401) {
+        print("Unauthorized: Invalid access token.");
+      } else if (response.statusCode == 500) {
+        print("Internal Server Error.");
+      } else {
+        throw Exception('Failed to load pet data');
+      }
+    } catch (e) {
+      print('Error loading pet data: $e');
     }
   }
 }
--- lib/screen/login/login_page.dart
+++ lib/screen/login/login_page.dart
@@ -5,7 +5,6 @@ import 'dart:io'; // 플랫폼 확인
 import 'dart:convert';
 import 'package:http/http.dart' as http;
 import 'package:flutter_secure_storage/flutter_secure_storage.dart'; // 보안 저장소
-import '../main/main_page.dart';
 
 class LoginPage extends StatefulWidget {
   const LoginPage({Key? key}) : super(key: key);
@@ -15,11 +14,9 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
-  final WebViewCookieManager _cookieManager = WebViewCookieManager();
-  late WebViewController _webViewController;
-  late WebviewController _windowsWebViewController;
   final FlutterSecureStorage _secureStorage = FlutterSecureStorage(); // 보안 저장소
-
+  WebViewController? _androidWebViewController; // Android WebView Controller
+  WebviewController? _windowsWebViewController; // Windows WebView Controller
   bool _isWebViewVisible = false;
 
   final String naverServerUrl = 'http://223.130.162.100:4525/login/request/naver';
@@ -29,97 +26,92 @@ class _LoginPageState extends State<LoginPage> {
   void initState() {
     super.initState();
     if (Platform.isAndroid) {
-      WidgetsFlutterBinding.ensureInitialized();
-      _webViewController = WebViewController()
-        ..setJavaScriptMode(JavaScriptMode.unrestricted)
-        ..setNavigationDelegate(
-          NavigationDelegate(
-            onNavigationRequest: (NavigationRequest request) {
-              if (request.url.startsWith(
-                  "http://223.130.162.100:4525/login/oauth2/code/naver")) {
-                _handleRedirectUri(request.url, isNaver: true);
-                return NavigationDecision.prevent;
-              } else if (request.url.startsWith(
-                  "http://223.130.162.100:4525/login/oauth2/code/kakao")) {
-                _handleRedirectUri(request.url, isNaver: false);
-                return NavigationDecision.prevent;
-              }
-              return NavigationDecision.navigate;
-            },
-          ),
-        );
+      _initializeAndroidWebView();
     } else if (Platform.isWindows) {
-      _windowsWebViewController = WebviewController();
       _initializeWindowsWebView();
     }
   }
 
+  void _initializeAndroidWebView() {
+    _androidWebViewController = WebViewController()
+      ..setJavaScriptMode(JavaScriptMode.unrestricted)
+      ..setNavigationDelegate(
+        NavigationDelegate(
+          onNavigationRequest: (NavigationRequest request) {
+            if (request.url.startsWith("http://223.130.162.100:4525/login/oauth2/code/naver")) {
+              _handleRedirectUri(request.url, isNaver: true);
+              return NavigationDecision.prevent;
+            } else if (request.url.startsWith("http://223.130.162.100:4525/login/oauth2/code/kakao")) {
+              _handleRedirectUri(request.url, isNaver: false);
+              return NavigationDecision.prevent;
+            }
+            return NavigationDecision.navigate;
+          },
+        ),
+      );
+  }
+
   Future<void> _initializeWindowsWebView() async {
+    _windowsWebViewController = WebviewController();
     try {
-      await _windowsWebViewController.initialize();
+      await _windowsWebViewController!.initialize();
+      _windowsWebViewController!.url.listen((url) {
+        if (url != null && url.startsWith("http://223.130.162.100:4525/login/oauth2/code")) {
+          if (url.contains("naver")) {
+            _handleRedirectUri(url, isNaver: true);
+          } else if (url.contains("kakao")) {
+            _handleRedirectUri(url, isNaver: false);
+          }
+        }
+      });
+      print("Windows WebView 초기화 성공");
     } catch (e) {
       print("Windows WebView 초기화 실패: $e");
     }
   }
 
-  void _loadNaverLoginPage() async {
-    final sessionCookie = await _secureStorage.read(key: 'session_cookie'); // 세션 쿠키 읽기
+  void _loadWebView(String url) {
+    setState(() {
+      _isWebViewVisible = true;
+    });
 
-    try {
-      final response = await http.get(
-        Uri.parse(naverServerUrl),
-        headers: {
-          if (sessionCookie != null) 'Cookie': 'JSESSIONID=$sessionCookie', // 세션 쿠키 포함
-        },
-      );
-      print("Naver Login API Response: Status Code: ${response.statusCode}");
-      print("Naver Login API Response: Body: ${response.body}");
-      if (response.statusCode == 200) {
-        final naverLoginUrl = response.body; // 서버에서 반환된 URL
-        _loadWebView(naverLoginUrl); // WebView로 로드
-      } else {
-        print('네이버 로그인 URL 로딩 실패: ${response.statusCode}');
-      }
-    } catch (e) {
-      print('네이버 로그인 요청 중 오류 발생: $e');
+    if (Platform.isAndroid && _androidWebViewController != null) {
+      _androidWebViewController!.loadRequest(Uri.parse(url));
+    } else if (Platform.isWindows && _windowsWebViewController != null) {
+      _windowsWebViewController!.loadUrl(url);
     }
   }
 
-  void _loadKakaoLoginPage() async {
-    final sessionCookie = await _secureStorage.read(key: 'session_cookie'); // 세션 쿠키 읽기
-    print('Saved Session Cookie: $sessionCookie');
+  void _loadLoginPage(String serverUrl) async {
+    final sessionCookie = await _secureStorage.read(key: 'session_cookie');
     try {
       final response = await http.get(
-        Uri.parse(kakaoServerUrl),
+        Uri.parse(serverUrl),
         headers: {
-          if (sessionCookie != null) 'Cookie': 'JSESSIONID=$sessionCookie', // 세션 쿠키 포함
+          if (sessionCookie != null) 'Cookie': 'JSESSIONID=$sessionCookie',
         },
       );
-      print("Kakao Login API Response: Status Code: ${response.statusCode}");
-      print("Kakao Login API Response: Body: ${response.body}");
       if (response.statusCode == 200) {
-        final kakaoLoginUrl = response.body; // 서버에서 반환된 URL
-        _loadWebView(kakaoLoginUrl); // WebView로 로드
+        final loginUrl = response.body;
+        _loadWebView(loginUrl);
       } else {
-        print('카카오 로그인 URL 로딩 실패: ${response.statusCode}');
+        print('로그인 URL 로딩 실패: ${response.statusCode}');
       }
     } catch (e) {
-      print('카카오 로그인 요청 중 오류 발생: $e');
+      print('로그인 요청 중 오류 발생: $e');
     }
   }
+
+  void _loadNaverLoginPage() => _loadLoginPage(naverServerUrl);
+  void _loadKakaoLoginPage() => _loadLoginPage(kakaoServerUrl);
 
   void _handleRedirectUri(String url, {required bool isNaver}) async {
-    print("Handling Redirect for URL: $url");
-    if (Platform.isWindows) {
-      await _windowsWebViewController.dispose();
-    }
     setState(() {
       _isWebViewVisible = false;
     });
 
+    final sessionCookie = await _secureStorage.read(key: 'session_cookie');
     try {
-      final sessionCookie = await _secureStorage.read(key: 'session_cookie');
-
       final response = await http.post(
         Uri.parse(url),
         headers: {
@@ -127,12 +119,8 @@ class _LoginPageState extends State<LoginPage> {
           if (sessionCookie != null) 'Cookie': 'JSESSIONID=$sessionCookie',
         },
       );
-      print("Redirect API Response: Status Code: ${response.statusCode}");
-      print("Redirect API Response: Body: ${response.body}");
-      print("Request Headers: ${response.request?.headers}");
       if (response.statusCode == 200) {
         final data = jsonDecode(response.body);
-        print("Parsed Data from Redirect Response: $data");
         await _saveUserData(
           id: data['id'],
           name: data['name'],
@@ -141,33 +129,12 @@ class _LoginPageState extends State<LoginPage> {
           isNew: response.headers['isNew'] == "1",
           isNaver: isNaver,
         );
-
-        print('${isNaver ? "네이버" : "카카오"} 로그인 성공: ID=${data['id']}, 토큰=${data['access_token']}');
         Navigator.pushReplacementNamed(context, '/main');
       } else {
         print('로그인 완료 후 토큰 데이터 요청 실패: ${response.statusCode}');
       }
     } catch (e) {
       print('Redirect 처리 중 오류 발생: $e');
-    }
-  }
-
-  void _loadWebView(String url) {
-    if (Platform.isAndroid) {
-      setState(() {
-        _isWebViewVisible = true;
-      });
-      _webViewController.loadRequest(Uri.parse(url));
-    } else if (Platform.isWindows) {
-      _windowsWebViewController.loadUrl(url);
-      _windowsWebViewController.url.listen((String? redirectedUrl) {
-        if (redirectedUrl != null &&
-            redirectedUrl.startsWith(
-                "http://223.130.162.100:4525/login/oauth2/code")) {
-          _handleRedirectUri(redirectedUrl,
-              isNaver: redirectedUrl.contains("naver"));
-        }
-      });
     }
   }
 
@@ -185,13 +152,7 @@ class _LoginPageState extends State<LoginPage> {
     await _secureStorage.write(key: 'accessToken', value: accessToken);
     await _secureStorage.write(key: 'isNew', value: isNew.toString());
     await _secureStorage.write(key: 'provider', value: isNaver ? 'naver' : 'kakao');
-
     print('사용자 데이터가 저장되었습니다.');
-  }
-
-  Future<void> _clearCookiesAndCache() async {
-    await _cookieManager.clearCookies();
-    print('쿠키와 캐시가 삭제되었습니다.');
   }
 
   @override
@@ -211,75 +172,72 @@ class _LoginPageState extends State<LoginPage> {
         child: Stack(
           children: [
             Center(
-              child: Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 24.0),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    const Text(
-                      'ECO Buddy',
-                      style: TextStyle(
-                        fontSize: 32,
-                        fontWeight: FontWeight.bold,
-                        color: Colors.green,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Text(
+                    'ECO Buddy',
+                    style: TextStyle(
+                      fontSize: 32,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.green,
+                    ),
+                  ),
+                  const SizedBox(height: 8),
+                  Text(
+                    '디지털 탄소 발자국을 줄이는 여정을 시작하세요!',
+                    style: TextStyle(fontSize: 16, color: Colors.grey[700]),
+                  ),
+                  const SizedBox(height: 40),
+                  ElevatedButton.icon(
+                    onPressed: _loadKakaoLoginPage,
+                    icon: Image.asset(
+                      'assets/images/icon/kakao_icon.png',
+                      width: 24,
+                      height: 24,
+                    ),
+                    label: const Text(
+                      '카카오 로그인',
+                      style: TextStyle(color: Colors.black, fontSize: 16),
+                    ),
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: const Color(0xFFFEE500),
+                      elevation: 0,
+                      minimumSize: const Size(200, 50),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(8.0),
                       ),
                     ),
-                    const SizedBox(height: 8),
-                    Text(
-                      '디지털 탄소 발자국을 줄이는 여정을 시작하세요!',
-                      style: TextStyle(fontSize: 16, color: Colors.grey[700]),
+                  ),
+                  const SizedBox(height: 16),
+                  ElevatedButton.icon(
+                    onPressed: _loadNaverLoginPage,
+                    icon: Image.asset(
+                      'assets/images/icon/naver_icon.png',
+                      width: 24,
+                      height: 24,
                     ),
-                    const SizedBox(height: 40),
-                    ElevatedButton.icon(
-                      onPressed: _loadKakaoLoginPage,
-                      icon: Image.asset(
-                        'assets/images/icon/kakao_icon.png',
-                        width: 24,
-                        height: 24,
-                      ),
-                      label: const Text(
-                        '카카오 로그인',
-                        style: TextStyle(color: Colors.black, fontSize: 16),
-                      ),
-                      style: ElevatedButton.styleFrom(
-                        backgroundColor: const Color(0xFFFEE500),
-                        elevation: 0,
-                        minimumSize: const Size(200, 50),
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(8.0),
-                        ),
+                    label: const Text(
+                      '네이버 로그인',
+                      style: TextStyle(color: Colors.white, fontSize: 16),
+                    ),
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: const Color(0xFF03C75A),
+                      elevation: 0,
+                      minimumSize: const Size(200, 50),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(8.0),
                       ),
                     ),
-                    const SizedBox(height: 16),
-                    ElevatedButton.icon(
-                      onPressed: _loadNaverLoginPage,
-                      icon: Image.asset(
-                        'assets/images/icon/naver_icon.png',
-                        width: 24,
-                        height: 24,
-                      ),
-                      label: const Text(
-                        '네이버 로그인',
-                        style: TextStyle(color: Colors.white, fontSize: 16),
-                      ),
-                      style: ElevatedButton.styleFrom(
-                        backgroundColor: const Color(0xFF03C75A),
-                        elevation: 0,
-                        minimumSize: const Size(200, 50),
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(8.0),
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
+                  ),
+                ],
               ),
             ),
             if (_isWebViewVisible && Platform.isAndroid)
-              WebViewWidget(controller: _webViewController),
+              WebViewWidget(controller: _androidWebViewController!),
             if (_isWebViewVisible && Platform.isWindows)
               Positioned.fill(
-                child: Webview(_windowsWebViewController),
+                child: Webview(_windowsWebViewController!),
               ),
           ],
         ),
